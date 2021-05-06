48c26956becaaa681a8debc5c6cfb7fd355e5d00
Gøran Berntsen <goran.berntsen@nav.no>
2021-05-06 13:23:38 +0200

    init
    Co-authored-by: Audun Fauchald Strand <audun.fauchald.strand@nav.no>
    Co-authored-by: Truls Jørgensen <truls.jorgensen@nav.no>

---
+++ src/main/kotlin/io/nais/devrapid/Configuration.kt
@@ -0,0 +1,37 @@
+package io.nais.devrapid
+
+import com.natpryce.konfig.ConfigurationProperties
+import com.natpryce.konfig.ConfigurationProperties.Companion.systemProperties
+import com.natpryce.konfig.EnvironmentVariables
+import com.natpryce.konfig.Key
+import com.natpryce.konfig.overriding
+import com.natpryce.konfig.stringType
+import java.util.*
+
+private fun config() =
+    systemProperties() overriding EnvironmentVariables overriding ConfigurationProperties.fromResource("defaults.properties")
+
+data class Configuration(
+    val topic:String = "aura.dev-rapid"
+    )
+
+
+fun createKafkaConfig(): Properties {
+    val props = Properties()
+    val schemaRegistryUser = config()[Key("KAFKA_SCHEMA_REGISTRY_USER", stringType)]
+    val schemaRegistryPassword = config()[Key("KAFKA_SCHEMA_REGISTRY_PASSWORD", stringType)]
+    props["bootstrap.servers"] = config()[Key("KAFKA_BROKERS", stringType)]
+    props["security.protocol"] = "SSL"
+    props["schema.registry.url"] = config()[Key("KAFKA_SCHEMA_REGISTRY", stringType)]
+    props["basic.auth.credentials.source"] = "USER_INFO"
+    props["basic.auth.user.info"] = "$schemaRegistryUser:$schemaRegistryPassword"
+    props["ssl.truststore.location"] = config()[Key("KAFKA_TRUSTSTORE_PATH", stringType)]
+    props["ssl.truststore.password"] = config()[Key("KAFKA_CREDSTORE_PASSWORD", stringType)]
+    props["ssl.keystore.type"] = "PKCS12"
+    props["ssl.keystore.location"] = config()[Key("KAFKA_KEYSTORE_PATH", stringType)]
+    props["ssl.keystore.password"] = config()[Key("KAFKA_CREDSTORE_PASSWORD", stringType)]
+    props["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
+    props["value.serializer"] = "org.apache.kafka.common.serialization.ByteArraySerializer"
+    return props
+}
+
