e7c37b00d935c16d3c901b63752f2a9a63c81522
Truls Jørgensen <trulsjor@gmail.com>
2021-05-10 15:55:12 +0200

    Calculated lead time
    Co-authored-by: Audun Fauchald Strand <audun.fauchald.strand@nav.no>
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>

--- src/main/kotlin/io/nais/devrapid/LeadtimeCalculator.kt
+++ src/main/kotlin/io/nais/devrapid/LeadtimeCalculator.kt
@@ -2,18 +2,25 @@ package io.nais.devrapid
 
 import com.google.protobuf.InvalidProtocolBufferException
 import io.nais.devrapid.github.Message
+import io.prometheus.client.Gauge
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 import no.nav.protos.deployment.DeploymentEvent
+import no.nav.protos.deployment.DeploymentEvent.RolloutStatus
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.slf4j.LoggerFactory
 import java.time.Duration
-import java.util.*
 
 class LeadtimeCalculator(val configuration: Configuration) {
 
     private val consumer = KafkaConsumer<String, ByteArray>(configuration.props)
-
+    private val leadTimeGauge =
+        Gauge.build()
+            .name("deployment_leadtime")
+            .labelNames("repo")
+            .help("Lead time from Github push to completed deployment")
+            .create()
     private val LOGGER = LoggerFactory.getLogger("devrapid-leadtime")
 
 
@@ -25,41 +32,47 @@ class LeadtimeCalculator(val configuration: Configuration) {
 
             while (true) {
                 val records = consumer.poll(Duration.ofSeconds(1))
-
                 records.iterator().forEach {
-
-                    val push = try{
-                        Message.Push.parseFrom(it.value())
-                    }catch (e: InvalidProtocolBufferException) {
-                        null
+                    val push = it.parsePushMessage()
+                    push?.let { messages[push.latestCommitSha] = push }
+                    if (push == null) {
+                        val deploy = it.parseDeploymentEvent()
+                        deploy?.let {
+                            val key = deploy.gitCommitSha
+                            if (deploy.rolloutStatus == RolloutStatus.complete && messages.containsKey(key)) {
+                                computeLeadTime(messages[key]!!, deploy)
+                                messages.remove(key)
+                            }
+                        }
                     }
-                    messages.put(push.latestCommitSha, push)
-
-
-                    val deploy = try{
-                        DeploymentEvent.Event.parseFrom(it.value())
-                    }catch (e: InvalidProtocolBufferException) {
-                        null
-                    }
-
-
-                    if (messages.containsKey(deploy.gitCommitSha)) {
-                        computeLeadTime(messages[deploy.gitCommitSha], deploy)
-                    }
-
-
-
                 }
-
             }
         }
     }
+
+    private fun computeLeadTime(push: Message.Push, deploy: DeploymentEvent.Event) {
+        val leadTime = deploy.timestamp.seconds - push.webHookRecieved.seconds.toDouble()
+        LOGGER.info("Lead time for ${push.repositoryName} is $leadTime")
+        leadTimeGauge.labels(push.repositoryName).set(leadTime)
+
+
+    }
 }
 
+private fun ConsumerRecord<String, ByteArray>.parsePushMessage(): Message.Push? {
+    return try {
+        Message.Push.parseFrom(this.value())
+    } catch (e: InvalidProtocolBufferException) {
+        null
+    }
+}
 
-
-fun Pair<Message.Push, DeploymentEvent.Event>.isComplete(): Boolean {
-    return this.first != null && this.second != null
+private fun ConsumerRecord<String, ByteArray>.parseDeploymentEvent(): DeploymentEvent.Event? {
+    return try {
+        DeploymentEvent.Event.parseFrom(this.value())
+    } catch (e: InvalidProtocolBufferException) {
+        null
+    }
 }
 
 
