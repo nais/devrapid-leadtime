9885c700142d3a9dac513b89de9b2930a61e7353
Truls Jørgensen <trulsjor@gmail.com>
2021-05-12 20:31:32 +0200

    A bit more refactoring. Tried to separate memory for making migrations to a future persistent storage easier, but rolled back. Premature optimalization.

--- src/main/kotlin/io/nais/devrapid/EventCollector.kt
+++ src/main/kotlin/io/nais/devrapid/EventCollector.kt
@@ -4,55 +4,59 @@ import com.google.protobuf.Any
 import io.nais.devrapid.github.Message
 import io.prometheus.client.Gauge
 import no.nav.protos.deployment.DeploymentEvent
+import no.nav.protos.deployment.DeploymentEvent.RolloutStatus.complete
 import org.slf4j.LoggerFactory
 
 class EventCollector {
+
     private val memory = mutableMapOf<String, Message.Push>()
     private val LOGGER = LoggerFactory.getLogger("devrapid-leadtime")
+
     private val leadTimeGauge = Gauge.build()
         .name("deployment_leadtime")
         .labelNames("repo")
         .help("Lead time from Github push to completed deployment")
         .create()
 
-    private val messageMapSize = Gauge.build()
+    private val memorySize = Gauge.build()
         .name("message_map_size")
         .help("Size of map that holds messages (deploy and push)")
         .create()
+
 
     internal fun collectOrComputeLeadTime(byteArray: ByteArray) {
         val any = Any.parseFrom(byteArray)
         when {
             any.`is`(Message.Push::class.java) -> {
                 val push = any.unpack(Message.Push::class.java)
-
                 LOGGER.info("Received push message (repo: ${push.repositoryName} sha: ${push.latestCommitSha})")
                 memory[push.latestCommitSha] = push
-                messageMapSize.set(memory.keys.size.toDouble())
+                updateMemorySizeGauge()
             }
             any.`is`(DeploymentEvent.Event::class.java) -> {
                 val deploy = any.unpack(DeploymentEvent.Event::class.java)
-                val key = deploy.gitCommitSha
-                if (deploy.rolloutStatus == DeploymentEvent.RolloutStatus.complete && memory.containsKey(key)) {
-                    LOGGER.info("Received deploy message (app: ${deploy.application} sha: ${key})")
-                    computeLeadTime(memory[key]!!, deploy)
-                    memory.remove(key)
-                    messageMapSize.set(memory.keys.size.toDouble())
-                }else{
-                    LOGGER.info("Received deploy message (app: ${deploy.application} sha: ${key}) with status ${deploy.rolloutStatus}")
+                val sha = deploy.gitCommitSha
+                val push = memory[sha]
+                if (push != null && deploy.rolloutStatus == complete) {
+                    LOGGER.info("Received deploy message (app: ${deploy.application} sha: ${sha})")
+                    computeLeadTime(push, deploy)
+                    memory.remove(sha)
+                    updateMemorySizeGauge()
+                } else {
+                    LOGGER.info("Received deploy message (app: ${deploy.application} sha: ${sha}) with incomplete status: ${deploy.rolloutStatus}")
                 }
-            }
-            else -> {
             }
         }
     }
 
     private fun computeLeadTime(push: Message.Push, deploy: DeploymentEvent.Event) {
-        val leadTime = deploy.timestamp.seconds - push.webHookRecieved.seconds.toDouble()
-        LOGGER.info("Lead time for ${push.repositoryName} is $leadTime")
-        leadTimeGauge.labels(push.repositoryName).set(leadTime)
-
+        val leadTime = deploy.timestamp.seconds - push.webHookRecieved.seconds
+        LOGGER.info("Calculated lead time for deploy with sha ${push.latestCommitSha} in repo ${push.repositoryName} is $leadTime seconds")
+        leadTimeGauge.labels(push.repositoryName).set(leadTime.toDouble())
     }
 
-    fun memorySize() = memory.size
+    private fun updateMemorySizeGauge() = memorySize.set(memory.keys.size.toDouble())
+    internal fun memorySize() = memory.size
+
+
 }
--- src/test/kotlin/io/nais/devrapid/EventCollectorTest.kt
+++ src/test/kotlin/io/nais/devrapid/EventCollectorTest.kt
@@ -8,10 +8,18 @@ internal class EventCollectorTest {
 
 
     @Test
-    fun `should parse`() {
+    fun `should correlate push and deploy events`() {
         val collector = EventCollector()
         collector.collectOrComputeLeadTime(anyPushMessageProto(sha = "1").toByteArray())
+        collector.collectOrComputeLeadTime(anyPushMessageProto(sha = "2").toByteArray())
+        assertThat(collector.memorySize()).isEqualTo(2)
+        collector.collectOrComputeLeadTime(anyDeploymentProto(sha = "1", rolloutStatus = RolloutStatus.complete).toByteArray())
         assertThat(collector.memorySize()).isEqualTo(1)
+    }
+
+    @Test
+    fun `when deploy event is collected without correponding push, the event is dropped`() {
+        val collector = EventCollector()
         collector.collectOrComputeLeadTime(anyDeploymentProto(sha = "1", rolloutStatus = RolloutStatus.complete).toByteArray())
         assertThat(collector.memorySize()).isEqualTo(0)
     }
