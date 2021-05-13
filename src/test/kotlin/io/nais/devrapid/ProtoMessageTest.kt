97c2ff74d7d3f4ab53030b7ec8a69615b82227e6
Audun Fauchald Strand <audun.fauchald.strand@nav.no>
2021-05-13 12:30:38 +0200

    fixed test

--- src/test/kotlin/io/nais/devrapid/ProtoMessageTest.kt
+++ src/test/kotlin/io/nais/devrapid/ProtoMessageTest.kt
@@ -15,14 +15,15 @@ class ProtoMessageTest{
         val anyByteArray = Any.parseFrom(pushMessageProto.toByteArray())
         assertTrue(anyByteArray.`is`((Message.Push::class.java)))
         assertFalse(anyByteArray.`is`((DeploymentEvent.Event::class.java)))
-        assertThat(anyByteArray.unpack(Message.Push::class.java)).isEqualTo(pushMessageProto)
+        assertThat(anyByteArray.unpack(Message.Push::class.java)).isEqualTo(pushMessageProto.unpack(Message.Push::class.java))
 
         val deploymentProto = anyDeploymentProto()
         val anyDeploymentByteArray =  Any.parseFrom(deploymentProto.toByteArray())
         assertTrue(anyDeploymentByteArray.`is`((DeploymentEvent.Event::class.java)))
         assertFalse(anyDeploymentByteArray.`is`((Message.Push::class.java)))
-        assertThat(anyDeploymentByteArray.unpack(DeploymentEvent.Event::class.java)).isEqualTo(deploymentProto)
+        assertThat(anyDeploymentByteArray.unpack(DeploymentEvent.Event::class.java)).isEqualTo(deploymentProto.unpack(DeploymentEvent.Event::class.java))
 
     }
+
 }
 
